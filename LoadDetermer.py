9b3670eb45a1eac30e491202c5c31a26a3f813b1
I_Kostogrud <ikostogrud@gmail.com>
2021-02-03 18:12:26 +0700

    Update LoadDetermer.py
    Исправление багов от проверки линтерами

--- LoadDetermer.py
+++ LoadDetermer.py
@@ -1,8 +1,9 @@
 import psutil
 
-print (psutil.cpu_times())
-print (psutil.virtual_memory())
-print (psutil.virtual_memory().percent)
+
+print(psutil.cpu_times())
+print(psutil.virtual_memory())
+print(psutil.virtual_memory().percent)
 
 for proc in psutil.process_iter(['pid', 'name', 'username']):
     print(proc.info)
