ffcf59098675ee2d7e914630b2428a9f6560d346
IlyaKostogrud <40514150+IlyaKostogrud@users.noreply.github.com>
2021-02-03 18:21:23 +0700

    add "psutil" in project (#4)
    * add "psutil" in project
    
    * Update LoadDetermer.py
    
    Исправление багов от проверки линтерами
    
    * Update setup.cfg
    
    Игнор ошибок по print
    
    * Update setup.cfg

---
+++ LoadDetermer.py
@@ -0,0 +1,9 @@
+import psutil
+
+
+print(psutil.cpu_times())
+print(psutil.virtual_memory())
+print(psutil.virtual_memory().percent)
+
+for proc in psutil.process_iter(['pid', 'name', 'username']):
+    print(proc.info)
